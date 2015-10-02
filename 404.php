9f6ebe2c02c8bc51596fba499e4333c9923b27b2
shyling <450044101@qq.com>
2015-10-02 16:47:07 +0800

    remove unused and change 404 page

--- 404.php
+++ 404.php
@@ -4,6 +4,12 @@
         <title>您走入了一个未知区域</title>
     </head>
     <body>
-        <script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8"></script>
+        <script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="<?php $this->options->siteUrl(); ?>" homePageName="回到<?php $this->options->title(); ?>"></script>
+        <!
+        NULL String To Make File Size Big Than 512B.
+        NULL String To Make File Size Big Than 512B.
+        NULL String To Make File Size Big Than 512B
+        NULL String To Make File Size Big Than 512B
+        ->
     </body>
 </html>
